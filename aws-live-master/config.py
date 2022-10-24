72a1626e76cba8a3431ebfa996f09e3144654088
Kamran-7 <kamraansharief7@gmail.com>
2022-10-24 20:34:48 +0530

    saved code

---
+++ aws-live-master/config.py
@@ -0,0 +1,6 @@
+customhost = "testdatabase.cfhe96st5iij.ap-south-1.rds.amazonaws.com"
+customuser = "adminaws"
+custompass = "adminaws123"
+customdb = "testdatabase"
+custombucket = "addnames"
+customregion = "ap-south-1"
